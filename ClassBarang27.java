fb8f3dc93f13cc3bbcc4edfde5e6fdf5db3bd6af
Satrio Dian Nugroho <143977344+SATRIODIANNUGROHO@users.noreply.github.com>
2024-03-06 18:53:14 +0700

    Kelas Barang

---
+++ ClassBarang27.java
@@ -0,0 +1,12 @@
+public class ClassBarang27 {
+    public String Kode27;
+    public String Nama27;
+    public double Harga27;
+    public int Stok27;
+    public ClassBarang27 (String Kode, String Nama, double Harga, int Stok) {
+        Kode27 = Kode;
+        Nama27 = Nama;
+        Harga27 = Harga;
+        Stok27 = Stok;
+    }
+}
